5a81aa52a71b145646d6d519dbf0a16497071bd2
Kingsley Okeke <kingsley.okeke@beeksgroup.com>
2024-06-12 23:51:56 +0100

    add checks for hx-request header

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 from typing import Annotated
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form, Header, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -28,18 +28,38 @@ async def get_index(request: Request):
 
 
 @app.get("/todos", response_class=HTMLResponse)
-async def get_todos(request: Request, count: str):
-    context = {"todos": filter_todos(count), "count": todo_count()}
-    return templates.TemplateResponse(request, "responses/get-todos.html", context)
+async def get_todos(
+    request: Request,
+    count: str,
+    hx_request: Annotated[bool | None, Header()] = None,
+):
+    if hx_request:
+        # Compose and return HTMX response here
+        context = {"todos": filter_todos(count), "count": todo_count()}
+        return templates.TemplateResponse(request, "responses/get-todos.html", context)
+    else:
+        # Compose and return JSON response here
+        pass
 
 
 @app.post("/todo", response_class=HTMLResponse)
-async def create_todo(request: Request, description: Annotated[str, Form()]):
+async def create_todo(
+    request: Request,
+    description: Annotated[str, Form()],
+    hx_request: Annotated[bool | None, Header()] = None,
+):
     todo = Todo(description=description, id=int(time.time_ns() / 1000))
     todos.append(todo)
 
-    context = {"todos": todos, "count": todo_count(), "todo": todo}
-    return templates.TemplateResponse(request, "responses/create-todo.html", context)
+    if hx_request:
+        # Compose and return HTMX response here
+        context = {"todos": todos, "count": todo_count(), "todo": todo}
+        return templates.TemplateResponse(
+            request, "responses/create-todo.html", context
+        )
+    else:
+        # Compose and return JSON response here
+        pass
 
 
 @app.patch("/todo/{id}", response_class=HTMLResponse)
@@ -48,6 +68,7 @@ async def update_todo(
     id: int,
     description: Annotated[str | None, Form()] = None,
     completed: Annotated[bool | None, Form()] = None,
+    hx_request: Annotated[bool | None, Header()] = None,
 ):
     [index] = [i for (i, item) in enumerate(todos) if item.id == id]
 
@@ -57,54 +78,110 @@ async def update_todo(
     if completed is not None:
         todos[index].completed = not todos[index].completed
 
-    context = {"todos": todos, "count": todo_count(), "todo": todos[index]}
-    return templates.TemplateResponse(request, "responses/update-todo.html", context)
+    if hx_request:
+        # Compose and return HTMX response here
+        context = {"todos": todos, "count": todo_count(), "todo": todos[index]}
+        return templates.TemplateResponse(
+            request, "responses/update-todo.html", context
+        )
+    else:
+        # Compose and return JSON response here
+        pass
 
 
 @app.delete("/todo/{id}", response_class=HTMLResponse)
-async def delete_todo(request: Request, id: int):
+async def delete_todo(
+    request: Request,
+    id: int,
+    hx_request: Annotated[bool | None, Header()] = None,
+):
     [index] = [i for (i, item) in enumerate(todos) if item.id == id]
     todos.pop(index)
 
-    context = {"todos": todos, "count": todo_count()}
-    return templates.TemplateResponse(request, "responses/delete-todo.html", context)
+    if hx_request:
+        # Compose and return HTMX response here
+        context = {"todos": todos, "count": todo_count()}
+        return templates.TemplateResponse(
+            request, "responses/delete-todo.html", context
+        )
+    else:
+        # Compose and return JSON response here
+        pass
 
 
 @app.get("/todo/{id}/edit", response_class=HTMLResponse)
-async def edit_todo(request: Request, id: int):
+async def edit_todo(
+    request: Request,
+    id: int,
+    hx_request: Annotated[bool | None, Header()] = None,
+):
     [index] = [i for (i, item) in enumerate(todos) if item.id == id]
 
-    context = {"todos": todos, "count": todo_count(), "todo": todos[index]}
-    return templates.TemplateResponse(request, "responses/edit-todo.html", context)
+    if hx_request:
+        # Compose and return HTMX response here
+        context = {"todos": todos, "count": todo_count(), "todo": todos[index]}
+        return templates.TemplateResponse(request, "responses/edit-todo.html", context)
+    else:
+        # Compose and return JSON response here
+        pass
 
 
 @app.put("/todos/completed", response_class=HTMLResponse)
-async def mark_all_completed(request: Request, completed: Annotated[bool, Form()]):
+async def mark_all_completed(
+    request: Request,
+    completed: Annotated[bool, Form()],
+    hx_request: Annotated[bool | None, Header()] = None,
+):
     for todo in todos:
         todo.completed = completed
 
-    context = {"todos": todos, "count": todo_count()}
-    return templates.TemplateResponse(
-        request, "responses/mark-all-completed.html", context
-    )
+    if hx_request:
+        # Compose and return HTMX response here
+        context = {"todos": todos, "count": todo_count()}
+        return templates.TemplateResponse(
+            request, "responses/mark-all-completed.html", context
+        )
+    else:
+        # Compose and return JSON response here
+        pass
 
 
 @app.delete("/todos/completed", response_class=HTMLResponse)
-async def delete_all_completed(request: Request):
+async def delete_all_completed(
+    request: Request,
+    hx_request: Annotated[bool | None, Header()] = None,
+):
     todos[:] = [todo for todo in todos if todo.completed == False]
-    context = {"todos": todos, "count": todo_count()}
-    time.sleep(2)
-    return templates.TemplateResponse(
-        request, "responses/delete-all-completed.html", context
-    )
+
+    if hx_request:
+        # Compose and return HTMX response here
+        time.sleep(2)  # Simulate slow network to illustrate HTMX indicator
+        context = {"todos": todos, "count": todo_count()}
+        return templates.TemplateResponse(
+            request, "responses/delete-all-completed.html", context
+        )
+    else:
+        # Compose and return JSON response here
+        pass
 
 
 @app.put("/todo/{id}/cancel-edit", response_class=HTMLResponse)
-def cancel_edit(request: Request, id: int):
+def cancel_edit(
+    request: Request,
+    id: int,
+    hx_request: Annotated[bool | None, Header()] = None,
+):
     [index] = [i for (i, item) in enumerate(todos) if item.id == id]
 
-    context = {"todos": todos, "todo": todos[index], "count": todo_count()}
-    return templates.TemplateResponse(request, "responses/cancel-edit.html", context)
+    if hx_request:
+        # Compose and return HTMX response here
+        context = {"todos": todos, "todo": todos[index], "count": todo_count()}
+        return templates.TemplateResponse(
+            request, "responses/cancel-edit.html", context
+        )
+    else:
+        # Compose and return JSON response here
+        pass
 
 
 def todo_count() -> dict[str, int]:
